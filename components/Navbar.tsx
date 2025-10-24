103726303b1c64130ebbd77ad16a028510b512cc
Valentina Valverde <143435977+ValentinaValverde@users.noreply.github.com>
2025-10-23 23:58:20 -0400

    flesh out navbar links

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,17 @@
+import Link from 'next/link';
+
 export default function Navbar() {
   return (
-    <div className="w-screen border border-white p-6">
-      <p>nav</p>
+    <div
+      className="w-screen border border-white p-6 
+    flex flex-row justify-between items-center"
+    >
+      <Link href="/">Soffice Studio</Link>
+
+      <div className="flex flex-row gap-8">
+        <Link href="/about">About</Link>
+        <Link href="/contact">Contact</Link>
+      </div>
     </div>
   );
 }
