db64ccca78ad0a2b7f9a410dc4400e87a4f44743
Valentina Valverde <143435977+ValentinaValverde@users.noreply.github.com>
2025-10-24 00:00:44 -0400

    flesh out footer and navbar

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,17 @@
+import Link from 'next/link';
+
 export default function Footer() {
   return (
-    <div className="w-screen border border-white p-6">
-      <p>footer</p>
+    <div
+      className="w-screen border border-white p-6 
+    flex flex-row justify-between items-center"
+    >
+      <div className="flex flex-row gap-12">
+        <Link href="/about">About</Link>
+        <Link href="/contact">Contact</Link>
+      </div>
+
+      <p>Est. 2025</p>
     </div>
   );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,16 +2,8 @@ import Link from 'next/link';
 
 export default function Navbar() {
   return (
-    <div
-      className="w-screen border border-white p-6 
-    flex flex-row justify-between items-center"
-    >
+    <div className="w-screen border border-white p-6 text-center">
       <Link href="/">Soffice Studio</Link>
-
-      <div className="flex flex-row gap-8">
-        <Link href="/about">About</Link>
-        <Link href="/contact">Contact</Link>
-      </div>
     </div>
   );
 }
