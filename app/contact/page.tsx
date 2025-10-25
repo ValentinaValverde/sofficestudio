35e01de1f13fd158c9ff5b21fc2bd8103c9a36bf
Valentina Valverde <143435977+ValentinaValverde@users.noreply.github.com>
2025-10-25 03:38:16 -0400

    bare bones of contact page

---
+++ app/contact/page.tsx
@@ -0,0 +1,12 @@
+import MainBlock from '@/components/home/MainBlock';
+import Sidebar from '@/components/home/Sidebar';
+
+export default function Home() {
+  return (
+    <div className="flex flex-col md:flex-row justify-start flex-1 w-full">
+      <div className="w-full flex flex-1 border border-white p-6 ">
+        <h1 className="text-6xl">sofficestudio@gmail.com</h1>
+      </div>
+    </div>
+  );
+}
