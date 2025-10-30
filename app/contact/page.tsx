d026a4164cee956e1ab52650182fd5abdbf7cacf
Valentina Valverde <143435977+ValentinaValverde@users.noreply.github.com>
2025-10-30 13:36:52 -0400

    make sure email is responsive

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="flex flex-col md:flex-row justify-start flex-1 w-full">
       <div className="w-full flex flex-1 items-end border border-white p-6 ">
-        <h1 className="text-6xl">sofficestudio@gmail.com</h1>
+        <h1 className="text-6xl break-all">sofficestudio@gmail.com</h1>
       </div>
     </div>
   );
