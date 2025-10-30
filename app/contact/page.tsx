1eb8a98e31380546d330573f29bc0eeb1a7864b4
Valentina Valverde <143435977+ValentinaValverde@users.noreply.github.com>
2025-10-30 13:38:45 -0400

    remove unused imports

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,6 +1,3 @@
-import MainBlock from '@/components/home/MainBlock';
-import Sidebar from '@/components/home/Sidebar';
-
 export default function Home() {
   return (
     <div className="flex flex-col md:flex-row justify-start flex-1 w-full">
