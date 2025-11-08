31459249bf97a75e4d40300f02bad650de6c3a9c
Valentina Valverde <143435977+ValentinaValverde@users.noreply.github.com>
2025-11-08 16:20:59 -0500

    fix mobile layout for development page

--- app/development/page.tsx
+++ app/development/page.tsx
@@ -13,7 +13,7 @@ export default function page() {
       <div className="grid grid-cols-1 md:grid-cols-2">
         {developmentPageData.map((data, index) => {
           return (
-            <div key={index}>
+            <div key={index} className="col-span-2 md:col-span-1">
               <Block
                 title={data.title}
                 description={data.description}
