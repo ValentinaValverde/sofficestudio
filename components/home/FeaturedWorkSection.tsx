95ede585448145d09dc13f1e1bfcf03af6a4979c
Valentina Valverde <143435977+ValentinaValverde@users.noreply.github.com>
2025-12-29 21:31:21 -0500

    simplify featured section component

--- components/home/FeaturedWorkSection.tsx
+++ components/home/FeaturedWorkSection.tsx
@@ -1,6 +1,4 @@
 import { developmentPageData } from '@/lib/constants';
-import DesktopFeaturedWorkBlock from '@/components/development/DesktopFeaturedWorkBlock';
-import MobileFeaturedWorkBlock from '@/components/development/MobileFeaturedWorkBlock';
 
 export default function FeaturedWorkSection() {
   return (
@@ -12,8 +10,44 @@ export default function FeaturedWorkSection() {
       {developmentPageData.map((data, index) => {
         return (
           <a key={index} href={data.link}>
-            <DesktopFeaturedWorkBlock data={data} />
-            <MobileFeaturedWorkBlock data={data} />
+            {/* desktop */}
+            <div
+              className="hidden md:block group bg-black text-white border border-white
+            overflow-hidden transition-all duration-500 ease-out
+            max-h-[80px] hover:max-h-[500px] hover:bg-white hover:text-black p-6"
+            >
+              <div className="flex justify-between items-start">
+                <p>{data.title}</p>
+                <p className="opacity-0 transition-all duration-500 group-hover:opacity-100 link-underline">
+                  View Project
+                </p>
+              </div>
+
+              {/* hidden until hover: */}
+              <div className="flex justify-between items-end pt-36 opacity-0 transition-all duration-500 group-hover:opacity-100">
+                <p className="text-3xl max-w-1/2">
+                  Reimagined a full music analytics platform with modern UI,
+                  responsive layouts, and rebuilt core flows.
+                </p>
+
+                <img
+                  src={data.imageUrl}
+                  alt={data.description}
+                  className="w-[360px] border border-gray-200 rounded-sm"
+                />
+              </div>
+            </div>
+
+            {/* mobile */}
+            <div className="p-6 border border-white space-y-6 max-w-[100vw] block md:hidden">
+              <p>{data.title}</p>
+              <p className="text-3xl !text-mono">{data.description}</p>
+              <img
+                src={data.imageUrl}
+                alt={data.description}
+                className="border border-gray-200 rounded-sm"
+              />
+            </div>
           </a>
         );
       })}
--- lib/types.ts
+++
@@ -1,6 +0,0 @@
-export type DataType = {
-  title: string;
-  description: string;
-  link: string;
-  imageUrl: string;
-};
