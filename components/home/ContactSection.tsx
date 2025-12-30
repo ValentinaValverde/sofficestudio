0d3fa42b44485a068fdbbafa1dc3e26a5afb4fc8
Valentina Valverde <143435977+ValentinaValverde@users.noreply.github.com>
2025-12-29 21:26:52 -0500

    keep 6xl font size for mobile :)

--- components/home/ContactSection.tsx
+++ components/home/ContactSection.tsx
@@ -2,7 +2,7 @@ export default function ContactSection() {
   return (
     <div className="flex flex-col flex-col-reverse md:flex-row justify-between items-center min-h-[70vh]">
       <div className="p-6 border border-white h-full w-full md:w-1/2 space-y-6 flex flex-col justify-end">
-        <p className="text-4xl md:text-6xl !font-mono">
+        <p className="text-6xl !font-mono">
           Let&apos;s build something cool together.{' '}
         </p>
 
